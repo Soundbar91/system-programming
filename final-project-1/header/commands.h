82b554f090963c72a79251826c46edc19d380e76
신관규 <soundbar91@gmail.com>
2024-11-22 14:42:09 +0900

    ..

--- final-project-1/header/commands.h
+++ final-project-1/header/commands.h
@@ -23,11 +23,4 @@ DECLARE_CMDFUNC(ps);
 DECLARE_CMDFUNC(run);
 DECLARE_CMDFUNC(kill);
 
-int validate_and_get_realpath(const char *input_path, char *output_path);
-int parse_proc_entry(pid_t pid, my_proc *proc);
-void display_basic_ps(my_proc *procs, int count);
-void display_ps_with_env(my_proc *procs, int count);
-void display_process_tree(my_proc *procs, int count);
-void print_tree(my_proc *procs, int count, int pid, int depth);
-
 #endif
