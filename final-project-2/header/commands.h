5b5ef86b29ead8c58e59223f9993c3e2be84a698
신관규 <soundbar91@gmail.com>
2024-12-05 17:00:18 +0900

    mkdir 명령어 구현

--- final-project-2/header/commands.h
+++ final-project-2/header/commands.h
@@ -3,9 +3,14 @@
  * commands.h
  */
 
+#include <stdio.h>
+#include <string.h>
+
 #define MAX_HELP_COMMENT (128)
+#define MAX_PATH_SIZE (128)
 #define MAX_CMD_SIZE (32)
 
+// function structure
 typedef int (*cmd_func_t)(int argc, char **argv);
 typedef void (*usage_func_t)(void);
 
@@ -17,12 +22,16 @@ typedef struct cmd_t
     char comment[MAX_HELP_COMMENT];
 } cmd_t;
 
+// function list
 extern cmd_t cmd_list[] = {
     {"help", cmd_help, usage_help, "show usage, ex) help <command>"},
+    {"mkdir", cmd_mkdir, usage_mkdir, "create directory"},
 };
 
+// function macro
 #define DECLARE_CMDFUNC(str)              \
     int cmd_##str(int argc, char **argv); \
     void usage_##str(void)
 
 DECLARE_CMDFUNC(help);
+DECLARE_CMDFUNC(mkdir);
---
+++ final-project-2/source/commands/mkdir.c
@@ -0,0 +1,28 @@
+#include "commands.h"
+#include "utils.h"
+#include <sys/stat.h>
+
+#define MKDIR_MODE 0755
+
+int cmd_mkdir(int argc, char **argv)
+{
+    char real_path[MAX_PATH_SIZE];
+    if (argc == 2)
+    {
+        get_realpath(argv[1], real_path);
+        if (mkdir(real_path, MKDIR_MODE) < 0) {
+            perror(argv[0]);
+            return -1;
+        }
+        return 0;
+    }
+    else
+    {
+        return -2;
+    }
+}
+
+void usage_mkdir(void)
+{
+    printf("mkdir <director>%n");
+}
